2d5aab1b0380d2912cd8b3f1b26527cea7f32f43
MeltedButetr77 <MeltedButterGaming@gmail.com>
2024-01-24 23:58:45 +1030

    simplified logic for a better example

--- main.py
+++ main.py
@@ -5,63 +5,48 @@ pygame.init()
 gameWindow = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 
-keys = []
-key = None
 grid_size = 20
-keys_to_remove = []
-speed = 200  # snake moves every speed milliseconds
+move_interval = 200  # snake moves every speed milliseconds
 snake = [(0, 0)]
-pygame.time.set_timer(pygame.USEREVENT, speed)
-isAlive = True
-while True:
-    while isAlive:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.KEYDOWN:
-                keys.insert(0, event.key)
-                if event.key == pygame.K_f:
-                    snake.insert(0, snake[-1])
-            if event.type == pygame.KEYUP:
-                keys_to_remove.append(event.key)
+pygame.time.set_timer(pygame.USEREVENT, move_interval)
+snake_direction = 'right'
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s:
+                snake_direction = 'down'
+            if event.key == pygame.K_w:
+                snake_direction = 'up'
+            if event.key == pygame.K_a:
+                snake_direction = 'left'
+            if event.key == pygame.K_d:
+                snake_direction = 'right'
 
-            if event.type == pygame.USEREVENT:
-                # Store last wasd key as key
-                if len(keys) > 0:
-                    opposite_keys = {pygame.K_w: pygame.K_s, pygame.K_s: pygame.K_w,
-                                     pygame.K_a: pygame.K_d, pygame.K_d: pygame.K_a}
-                    # If the key is one of the specified ones and is not the opposite of the old key
-                    if keys[0] in opposite_keys and keys[0] != opposite_keys.get(key, None):
-                        key = keys[0]
-                    for thing in keys_to_remove:
-                        print(keys, keys_to_remove)
-                        keys.remove(thing)
-                    keys_to_remove = []
+            if event.key == pygame.K_f:
+                snake.insert(0, snake[-1])
 
-                x, y = snake[-1][0], snake[-1][1]
-                if key == pygame.K_w:
-                    y -= grid_size
-                    print("up")
-                if key == pygame.K_s:
-                    y += grid_size
-                    print("down")
-                if key == pygame.K_d:
-                    x += grid_size
-                    print("right")
-                if key == pygame.K_a:
-                    x -= grid_size
-                    print("left")
-                # Adds new snake to the end of the list, head in -1 index
-                snake.append((x, y))
-                snake.pop(0)
+        if event.type == pygame.USEREVENT:
+            # Get current head
+            x, y = snake[-1][0], snake[-1][1]
+            if snake_direction == 'down':
+                snake.append((x, y + grid_size))
+            if snake_direction == 'up':
+                snake.append((x, y - grid_size))
+            if snake_direction == 'left':
+                snake.append((x - grid_size, y))
+            if snake_direction == 'right':
+                snake.append((x + grid_size, y))
+            snake.pop(0)
 
-        head_rect = pygame.Rect(snake[-1], (grid_size, grid_size))
+    for loc in snake:
+        pygame.draw.rect(gameWindow, (0, 0, 255), pygame.Rect(loc, (grid_size, grid_size)))
+    head_rect = pygame.Rect(snake[-1], (grid_size, grid_size))
+    pygame.draw.rect(gameWindow, (0, 255, 0), head_rect)
 
-        for loc in snake:
-            rect = pygame.Rect(loc, (grid_size, grid_size))
-            pygame.draw.rect(gameWindow, (0, 0, 255), rect)
-        pygame.draw.rect(gameWindow, (0, 255, 0), head_rect)
-        pygame.display.update()
-        gameWindow.fill((0, 0, 0))
-        clock.tick(60)
+    pygame.display.update()
+    gameWindow.fill((0, 0, 0))
+    clock.tick(60)
